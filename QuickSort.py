bbbef6bb61a3435cb5d96378a13aa70dd4aca141
Andy1iang <andyliang254@gmail.com>
2024-07-05 16:15:08 -0400

    Added Quick Sort

---
+++ QuickSort.py
@@ -0,0 +1,51 @@
+import UnitTests
+
+# Time Complexity: O(nlogn) - Worst Case: O(n^2)
+# Space Complexity: O(1)
+# Not Stable
+
+
+class QuickSort:
+
+    def __init__(self, arr):
+        self.arr = arr
+        self.sort()
+
+    def sort(self):
+        self._sort(0, len(self.arr)-1)
+
+    # recursive function
+    # sort by getting all pivot elements to their correct index
+    def _sort(self, low, high):
+
+        # Base Case: when there's not enough elements to sort
+        if low >= high:
+            return
+
+        pivotIndex = self.partition(low, high)  # partitioning
+        self._sort(low, pivotIndex-1)
+        self._sort(pivotIndex+1, high)
+
+    def partition(self, low, high):
+
+        # creating pivot element
+        pivot = (low+high)//2
+        pivotValue = self.arr[pivot]
+        self.swap(pivot, high)  # swapping it to the end temporarily
+
+        # checking all elements in the subsection until pivot is is it's correct location
+        # where all items to the left are smaller and to the right are larger
+        i = low
+        for j in range(low, high):
+            if self.arr[j] < pivotValue:
+                self.swap(i, j)
+                i += 1
+
+        self.swap(i, high)
+        return i
+
+    def swap(self, i, j):
+        self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
+
+
+UnitTests.runTestCases(QuickSort)
