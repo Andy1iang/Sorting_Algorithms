eefd86d4718d54c537dc59a3f35141401882df64
Andy1iang <andyliang254@gmail.com>
2024-07-06 00:51:02 -0400

    Added Merge Sort

---
+++ MergeSort.py
@@ -0,0 +1,53 @@
+import UnitTests
+
+# Time Complexity: O(nlogn)
+# Space Complexity: O(n)
+# Stable
+
+
+class MergeSort:
+
+    def __init__(self, arr):
+        self.arr = self.sort(arr)
+
+    def sort(self, arr):
+
+        # returns array when we can't split any more
+        # can also use insertion sort when we split small enough
+        if len(arr) <= 1:
+            return arr
+
+        # recursively splitting array
+        middleIndex = len(arr) // 2
+        left = arr[:middleIndex]
+        right = arr[middleIndex:]
+
+        # sorting each half
+        left = self.sort(left)
+        right = self.sort(right)
+
+        # merging sorted halves
+        i, j, k = 0, 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+        return arr
+
+
+UnitTests.runTestCases(MergeSort)
